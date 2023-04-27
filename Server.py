2fd144c46c589d7ceaeba9ee696f300a3d40881a
Code Key <105808889+CCodeKey@users.noreply.github.com>
2023-04-27 17:51:14 -0300

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,41 +1,73 @@
 import socket
 import openai
 
-
 HOST = '10.15.2.96'
 PORT = 12345
-
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind((HOST, PORT))
+print('Aguardando Perguntas ...')
+print()
 
-def pesquisar(pergunta):
-    openai.api_key = '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'
+def envio():
+    with open("C:\\Users\\Code Key\\Desktop\\Programas\\ChatGPT\\respotas\\resposta_a.txt","r") as text_A:
+        with open("C:\\Users\\Code Key\\Desktop\\Programas\\ChatGPT\\respotas\\resposta_b.txt","r") as text_B:
+            A = text_A.read()
+            B = text_B.read()
+            print("A -> ", A)
+            print("B ->", B)
+    end = (f"""
+Participante  A  ->  {A}
+
+
+Participante  B  ->  {B}
+    """)
+    resposta_do_chat = str(end).encode()
+    server_socket.sendto(resposta_do_chat, address)
+
+def Pessoa_A(pergunta):
+    openai.api_key = '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'
 
     def fazer_pergunta_no_chat(pergunta):
         engine = "text-davinci-002"
         prompt = f"Fazer uma pergunta: {pergunta}"
         completions = openai.Completion.create(engine=engine, prompt=prompt, max_tokens=1024)
-        resposta = completions.choices[0].text.strip()
-        return resposta
+        respostaCHT = completions.choices[0].text.strip()
+        return respostaCHT
 
-    # Exemplo de uso
     respostaChatGPT = fazer_pergunta_no_chat(pergunta)
-    print("Resposta do ChatGPT : ", respostaChatGPT)
-    # response = f'Resposta do servidor para {address}'.encode()
-    respostaChatGPT = respostaChatGPT.encode()
-    server_socket.sendto(respostaChatGPT, address)
+    with open("C:\\Users\\Code Key\\Desktop\\Programas\\ChatGPT\\respotas\\resposta_a.txt", "w") as resp:
+        resp.write(respostaChatGPT)
 
+def Pessoa_B(pergunta):
+    HOSTb = '10.15.2.237'
+    PORTb = 12345
+    clienteh_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-print('Aguardando Perguntas ...')
-print()
+    def Recebimento():
+        HOSTa = '10.15.2.96' 
+        PORTa = 29292
+        server_socket2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        server_socket2.bind((HOSTa, PORTa))
+        print("Esperando resposta !")
+        print()
+        for i in range(1):
+            data, address = server_socket2.recvfrom(1024)
+            RESPOSTA_DE_B = data.decode()
+            # envio_de_respostas_b(RESPOSTA_DE_B)
+            with open("C:\\Users\\Code Key\\Desktop\\Programas\\ChatGPT\\respotas\\resposta_b.txt", "w") as respt:
+                respt.write(RESPOSTA_DE_B)
+                envio()        
+
+    for i in range(1):
+        msg = pergunta
+        message = msg.encode()
+        clienteh_socket.sendto(message, (HOSTb, PORTb))
+        Recebimento()
 
 while True:
-    # Recebe dados e end  ereço do remetente
     data, address = server_socket.recvfrom(1024)
     print("Usuario > ", address)
     print("Pergunta > ", data.decode())
-    pesquisar(data.decode())
-
-    # Envia uma resposta para o remetente
-    # response = f'Resposta do servidor para {address}'.encode()
-    # server_socket.sendto(response, address)
+    pergunta = data.decode()
+    Pessoa_B(pergunta)
+    Pessoa_A(pergunta)
