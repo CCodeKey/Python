32d062db8e5731ac965ccd83094c774701907e48
Code Key <105808889+CCodeKey@users.noreply.github.com>
2023-04-27 17:50:34 -0300

    Update Cliente.py

--- Cliente.py
+++ Cliente.py
@@ -1,15 +1,12 @@
 import socket
 
-HOST = 'localhost' 
-PORT = 12345        
-
+HOST = '10.15.2.96' 
+PORT = 12345       
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-while True:
 
+while True:
     msg = input(str("> "))
     message = msg.encode()
     client_socket.sendto(message, (HOST, PORT))
     data, server_address = client_socket.recvfrom(1024)
-    print(f'Resposta do servidor ({server_address}): {data.decode()}')
-
-    # client_socket.close()
+    print(f'Resposta do servidor : {data.decode()}')
