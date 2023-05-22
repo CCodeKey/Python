b3f0502c2faa1a7f4ed2e80611178d307f9aac95
Code Key <105808889+CCodeKey@users.noreply.github.com>
2023-05-22 17:18:30 -0300

    Update fatorial.py

--- fatorial.py
+++ fatorial.py
@@ -1,20 +1,69 @@
-from math import factorial
 from collections import Counter
+from math import factorial
 
-def calculo(palavra):
+RED   = "\033[1;31m"  
+BLUE  = "\033[1;34m"
+CYAN  = "\033[1;36m"
+GREEN = "\033[0;32m"
+RESET = "\033[0;0m"
+BOLD    = "\033[;1m"
+REVERSE = "\033[;7m"
+
+def resultado(anagrama, repeticao):
+    if repeticao == 0:
+        print("    ##  Fórmula utilizada  ##")
+        print(f"{BOLD}{RED}          P  = {GREEN} n{RED}! {RESET}")
+        print()
+        print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+        print()
+        print("Resultado da Permutação Simples ",anagrama,".")
+    if repeticao >= 1:
+        print("    ##  Fórmula utilizada  ##")
+        print(f"{BLUE}           r {RESET}")
+        print(f"{BOLD}{RED}          P  = {GREEN} n{RED}! {RESET}")
+        print(f"{GREEN}           n   {BLUE} r! {RESET}")
+        print()
+        print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+        print()
+        print("- Resultado da Permutação Composta ",anagrama // repeticao,".")
+
+def calcular_anagramas(palavra):
     n = len(palavra)
-    contador = Counter(palavra)
-    repeticoes = 1
-    for letra, freq in contador.items():
-        repeticoes *= factorial(freq)
-    anagramas = factorial(n)
-    return anagramas, repeticoes
+    anagrama = factorial(n)
+    print(f"Nº de Anagramas    =  {n}! ou {anagrama}.")
+    return anagrama
 
-palavra = input("Digite uma palavra > ")
-anagramas, repeticoes = calculo(palavra)
-print(f"A palavra '{palavra}' possui :")
-print( f"{anagramas} anagramas;")
-print(f"{repeticoes} repetições.")
-print()
-print()
+def calcular_repeticoes(frase):
+    contador = Counter(frase)
+    repeticoes = {letra: quantidade for letra, quantidade in contador.items() if quantidade > 1}
+    return repeticoes
 
+while True:
+    print("    ##  Digite uma palavra  ##")
+    palavra = input("> ")
+    print()
+    print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+    print()
+    repeticoes = calcular_repeticoes(palavra)
+    num_anagramas = calcular_anagramas(palavra)
+    num_repeticoes = sum(repeticoes.values())
+    print(f"Nº de Repetições   =  {num_repeticoes}! ou {factorial(num_repeticoes)}.")
+    num_repeticoes = factorial(num_repeticoes)
+    print()
+    print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+    print()
+    resultado(num_anagramas , num_repeticoes)
+    print()
+    print("- Letras Repetidas :")
+    num = 0
+    for letra, quantidade in repeticoes.items():
+        print(f"     {letra}: {quantidade}") # or
+        # print(factorial(quantidade))
+        # som = factorial(quantidade) 
+    print()
+    print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+    print()
+    print()
+    print()
+    print()
+    print()
