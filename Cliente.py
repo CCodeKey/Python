5ddf668943b99a7b73b3b002cdd5033445b0bcd5
Code Key <105808889+CCodeKey@users.noreply.github.com>
2023-06-21 15:27:23 -0300

    Update Cliente.py

--- Cliente.py
+++ Cliente.py
@@ -1,17 +1,13 @@
 import socket
 
-HOST = '10.15.2.96' 
+HOST = '10.15.2.96' #IP servidor
 PORT = 12345       
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 while True:
     msg = input(str("> "))
-    print()
-    print()
-    print("   Aguardando Resposta !")
-    print()
-    print()
     message = msg.encode()
     client_socket.sendto(message, (HOST, PORT))
     data, server_address = client_socket.recvfrom(1024)
     print(f'Resposta do servidor : {data.decode()}')
+    print()
